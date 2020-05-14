06888814e9425f401c53873468cb0063aaadcd7b
Bhavya SIngh <bhavya4official@gmail.com>
2020-05-14 21:43:21 +0530

    Create Reverse_a_string.c

---
+++ Reverse_a_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char s[100],r[100];
+    int i,end,count=0;
+    printf("Input a string.\n");
+    fgets(s,sizeof(s),stdin);
+    while(s[count]!='\0')       //Calculating string length
+        count++;
+    end=count-1;
+    for(i=0;i<count;i++){
+        r[i]=s[end];
+        end--;
+    }
+    r[i]='\0';
+    printf("%s",r);
+    return 0;
+}
