083fc5c0aca5440194232932cc30df0d45647c00
Bhavya SIngh <bhavya4official@gmail.com>
2020-05-15 09:07:28 +0530

    Update Reverse_a_string.c

--- Reverse_a_string.c
+++ Reverse_a_string.c
@@ -4,7 +4,7 @@ int main(){
     int i,end,count=0;
     printf("Input a string.\n");
     fgets(s,sizeof(s),stdin);
-    while(s[count]!='\0')       //Calculating string length
+    while(s[count]!='\0')       //Calculating string length strlen(s)
         count++;
     end=count-1;
     for(i=0;i<count;i++){
